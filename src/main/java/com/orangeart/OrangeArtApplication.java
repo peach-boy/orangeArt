d6582b1ace1e4e0eb2fdabf730303bc9579174fd
xiantao.wu <xiantao.wu@guanaitong.com>
2021-03-13 21:52:03 +0800

    init

---
+++ src/main/java/com/orangeart/OrangeArtApplication.java
@@ -0,0 +1,13 @@
+package com.orangeart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orangeart/OrangeArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangeart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
